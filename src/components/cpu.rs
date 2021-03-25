30d991785b4ccf6529e88e017e1dbd0785eeb4a6
María Gil <cerrudo87@gmail.com>
2021-03-25 00:26:10 -0300

    Added module-level docs for cpu

--- src/components/cpu.rs
+++ src/components/cpu.rs
@@ -1,5 +1,47 @@
+//! # CHIP-8 Interpreter
+//! ## Description
+//! There are 35 different instructions in the most common implementation.
+//!
+//! Most of them contain values inside:
+//! * nnn refers to a hexadecimal address
+//! * nn refers to a hexadecimal byte
+//! * n refers to a hexadecimal nibble (half-byte)
+//! * x and y are 2 different registers in the same op
+//! Most of the time we will be incrementing the PC by one, unless when an instruction calls for a skip.
+//! ## Organization
+//! It is possible to order the instructions by looking at the most and least significant nibbles.
+//! This makes using a switch easier.
+//! ## Timer woes
+//! So, the CHIP-8 is not really an emulator, instead it is more of an interpreted language.
+//! What this means is that the clock speed is not a constant (Like a modern console) or set by hardware (Like the 8008, which was set by quartz crystal).
+//!
+//! Because of this, a bit of trial and error is needed to get a mostly correct timing.
+//! The "magic number" is often between 500hz and 600hz.
+//! ## The font utility
+//! As it is often necessary to print numbers on screen, CHIP-8 comes for an utility for it.
+//! The Dxyn instruction allows for quick output of the hex value of n to position (x,y).
+//!
+//! A list of the font characters can be found in the font constants, to be loaded to memory on constructor of Cpu.
+//! ## Binary Coded Decimal: What is bcd()
+//! Fx33 takes the binary coded decimal of the value in Vx and places it in I, I+1 and I+2.
+//! Taking the binary coded decimal of a number is simply spliting the digits of it into different places.
+//!
+//! ### For example:
+//!
+//! The number 114 (0b1110010 in binary) gets split the following way:
+//!
+//! ```
+//! mem[I] = 1
+//! mem[I + 1] = 1
+//! mem[I + 2] = 4
+//! ```
+//!
+//! This is used by the font utility to be able to display big numbers fast.
+
 use super::memory;
 use rand::Rng;
+
+/// Represents the processor, running instructions and sending orders to other modules
 pub struct Cpu {
     v: [u8; 16], /* Data registers
                   * The CHIP-8 interpreter has 16 general purpose data registers, V0 to VF
@@ -52,36 +94,7 @@ impl Default for Cpu {
     }
 }
 
-/*  # CHIP-8 Instruction Set
-*   ## Description
-*   There are 35 different instructions in the most common implementation
-*   Most of them contain values inside:
-*   * nnn refers to a hexadecimal address
-*   * nn refers to a hexadecimal byte
-*   * n refers to a hexadecimal nibble (half-byte)
-*   * x and y are 2 different registers in the same op
-*   Most of the time we will be incrementing the PC by one, unless when an instruction calls for a skip
-*   ## Organization
-*   It is possible to order the instructions by looking at the most and least significant nibbles
-*   This makes using a switch easier
-*   ## Timer woes
-*   So, the CHIP-8 is not really an emulator, instead it is more of an interpreted language
-*   What this means is that the clock speed is not a constant (Like a modern console) or set by hardware (Like the 8008, which was set by quartz crystal)
-*   Because of this, a bit of trial and error is needed to get a mostly correct timing
-*   The "magic number" is often between 500hz and 600hz
-*   ## The font utility
-*   As it is often necessary to print numbers on screen, CHIP-8 comes for an utility for it
-*   The Dxyn instruction allows for quick output of the hex value of n to position (x,y)
-    A list of the font characters can be found in the font constants, to be loaded to memory on constructor of Cpu
-*   ## Binary Coded Decimal: WTF is bcd()
-*   Fx33 takes the binary coded decimal of the value in Vx and places it in I, I+1 and I+2
-*   Taking the binary coded decimal of a number is simply spliting the digits of it into different places
-*   For example:
-*   The number 114 (0b1110010 in binary)
-*   Gets split the following way:
-*   [1, 1, 4]
-*   This is used by the font utility to be able to display big numbers
-*   ## Instructions
+/*  ## Instructions
 *   0nnn - Execute machine language subroutine at nnn
 *   00E0 - cls()
 *   00EE - Return from subroutine
@@ -138,6 +151,8 @@ impl Cpu {
         [0xF0, 0x80, 0xF0, 0x80, 0xF0], // E Done
         [0xF0, 0x80, 0xF0, 0x80, 0x80], // F Done
     ];
+
+    /// Used to load the fonts in the default location so that they can be used by Dxyn/draw_sprite()
     pub fn write_fonts_to_mem(mem: &mut memory::Memory) {
         for (idx, sprite) in Cpu::FONT.iter().flatten().enumerate() {
             let res = mem.unbound_write((idx + 0x20) as u16, *sprite);
@@ -147,120 +162,120 @@ impl Cpu {
             }
         }
     }
-    // 0nnn - Execute machine language subroutine at nnn
-    // Implemented same as 2nnn in this case
+    /// 0nnn - Execute machine language subroutine at nnn
+    /// Implemented same as 2nnn in this case
     fn ml_sub(&mut self, addr: u16) {
         self.call_sub(addr)
     }
-    // 0E00 - cls()
+    /// 0E00 - cls()
     fn cls(&self, state: &mut [[bool; 32]; 64]) {
         *state = [[false; 32]; 64];
     }
-    // 00EE - Return from subroutine
+    /// 00EE - Return from subroutine
     fn ret_sub(&mut self) {
         let popped_addr = self.stack.pop().unwrap();
         self.program_counter = popped_addr
     }
-    // 1nnn - Jump to nnn
+    /// 1nnn - Jump to nnn
     fn jump(&mut self, addr: u16) {
         self.program_counter = addr - 1;
     }
-    // 2nnn - Execute subroutine at nnn
+    /// 2nnn - Execute subroutine at nnn
     fn call_sub(&mut self, addr: u16) {
         self.stack.push(self.program_counter);
         self.program_counter = addr - 1
     }
-    // 3xnn - Skip if Vx == nn
+    /// 3xnn - Skip if Vx == nn
     fn if_reg_equals_nn(&mut self, x: u8, nn: u8) {
         let vx = self.v[x as usize];
         if vx == nn {
             self.program_counter = self.program_counter + 1
         }
     }
-    // 4xnn - Skip if Vx != nn
+    /// 4xnn - Skip if Vx != nn
     fn if_not_reg_equals_nn(&mut self, x: u8, nn: u8) {
         let vx = self.v[x as usize];
         if vx != nn {
             self.program_counter = self.program_counter + 1
         }
     }
-    // 5xy0 - Skip if Vx == Vy
+    /// 5xy0 - Skip if Vx == Vy
     fn if_reg_equals_reg(&mut self, x: u8, y: u8) {
         if self.v[x as usize] == self.v[y as usize] {
             self.program_counter = self.program_counter + 1
         }
     }
-    // 6xnn - Vx = nn
+    /// 6xnn - Vx = nn
     fn reg_store_nn(&mut self, x: u8, nn: u8) {
         self.v[x as usize] = nn
     }
-    // 7xnn - Vx = Vx + nn; CHECK OVERFLOW BEHAVIOR
+    /// 7xnn - Vx = Vx + nn; CHECK OVERFLOW BEHAVIOR
     fn reg_add_nn(&mut self, x: u8, nn: u8) {
         self.v[x as usize] = self.v[x as usize] + nn
     }
-    // 8xy0 - Vx = Vy
+    /// 8xy0 - Vx = Vy
     fn assign_reg_to_reg(&mut self, x: u8, y: u8) {
         self.v[x as usize] = self.v[y as usize]
     }
-    // 8xy1 - Vx = Vx | Vy
+    /// 8xy1 - Vx = Vx | Vy
     fn reg_or_reg(&mut self, x: u8, y: u8) {
         self.v[x as usize] = self.v[x as usize] | self.v[y as usize]
     }
-    // 8xy2 - Vx = Vx & Vy
+    /// 8xy2 - Vx = Vx & Vy
     fn reg_and_reg(&mut self, x: u8, y: u8) {
         self.v[x as usize] = self.v[x as usize] & self.v[y as usize]
     }
-    // 8xy3 - Vx = Vx ^ Vy
+    /// 8xy3 - Vx = Vx ^ Vy
     fn reg_xor_reg(&mut self, x: u8, y: u8) {
         self.v[x as usize] = self.v[x as usize] ^ self.v[y as usize]
     }
-    // 8xy4 - Vx = Vx + Vy; VF = Carry?
+    /// 8xy4 - Vx = Vx + Vy; VF = Carry?
     fn reg_plus_reg(&mut self, x: u8, y: u8) {
         let result = self.v[x as usize].overflowing_add(self.v[y as usize]);
         self.v[x as usize] = result.0;
         self.v[0xF] = result.1 as u8;
     }
-    // 8xy5 - Vx = Vx - Vy; VF = 0 if borrow else 1
+    /// 8xy5 - Vx = Vx - Vy; VF = 0 if borrow else 1
     fn reg_minus_reg(&mut self, x: u8, y: u8) {
         let result = self.v[x as usize].overflowing_sub(self.v[y as usize]);
         self.v[x as usize] = result.0;
         self.v[0xF] = (!result.1) as u8;
     }
-    // 8xy6 - Vx = Vy >> 1; VF = Vy & 1
+    /// 8xy6 - Vx = Vy >> 1; VF = Vy & 1
     fn reg_shift_right(&mut self, x: u8) {
         self.v[0xF] = self.v[x as usize] & 1;
         self.v[x as usize] = self.v[x as usize] >> 1;
     }
-    // 8xy7 - Vx = Vy - Vx; VF = Borrow?
+    /// 8xy7 - Vx = Vy - Vx; VF = Borrow?
     fn reverse_reg_minus_reg(&mut self, x: u8, y: u8) {
         let result = self.v[y as usize].overflowing_sub(self.v[x as usize]);
         self.v[x as usize] = result.0;
         self.v[0xF] = (!result.1) as u8;
     }
-    // 8xyE - Vx = Vy << 1; VF = Vy >> 7
+    /// 8xyE - Vx = Vy << 1; VF = Vy >> 7
     fn reg_shift_left(&mut self, x: u8) {
         self.v[0xF] = self.v[x as usize] >> 7;
         self.v[x as usize] = self.v[x as usize] << 1;
     }
-    // 9xy0 - Skip if Vx != Vy
+    /// 9xy0 - Skip if Vx != Vy
     fn if_not_reg_equals_reg(&mut self, x: u8, y: u8) {
         if self.v[x as usize] != self.v[y as usize] {
             self.program_counter = self.program_counter + 1
         }
     }
-    // Annn - I = nnn
+    /// Annn - I = nnn
     fn store_addr(&mut self, nnn: u16) {
         self.i = nnn;
     }
-    // Bnnn - Jump to nnn + V0
+    /// Bnnn - Jump to nnn + V0
     fn reg_plus_nnn_jump(&mut self, nnn: u16) {
         self.program_counter = self.v[0] as u16 + nnn - 1
     }
-    // Cxnn = Vx = Rand() & nn
+    /// Cxnn = Vx = Rand() & nn
     fn random(&mut self, x: u8, nn: u8) {
         self.v[x as usize] = self.rng.gen::<u8>() & nn;
     }
-    // Dxyn = draw(x: Vx, y: Vy, sprite: sprite(sprite_height: n, sprite_addr: I)); VF = Pixels unset?
+    /// Dxyn = draw(x: Vx, y: Vy, sprite: sprite(sprite_height: n, sprite_addr: I)); VF = Pixels unset?
     fn draw_sprite(
         &mut self,
         x: u8,
@@ -285,23 +300,23 @@ impl Cpu {
             }
         }
     }
-    // Ex9E = Skip if key_pressed(hex(Vx)) //keypad is formed by numbers in hex
+    /// Ex9E = Skip if key_pressed(hex(Vx)) //keypad is formed by numbers in hex
     fn if_key_pressed(&mut self, keys_pressed: &[bool; 16], x: u8) {
         if keys_pressed[self.v[x as usize] as usize] {
             self.program_counter = self.program_counter + 1
         }
     }
-    // ExA1 = Skip if !key_pressed(hex(Vx))
+    /// ExA1 = Skip if !key_pressed(hex(Vx))
     fn if_not_key_pressed(&mut self, keys_pressed: &[bool; 16], x: u8) {
         if !keys_pressed[self.v[x as usize] as usize] {
             self.program_counter = self.program_counter + 1
         }
     }
-    // Fx07 = Vx = dt
+    /// Fx07 = Vx = dt
     fn store_dt(&mut self, x: u8) {
         self.v[x as usize] = self.dt
     }
-    // Fx0A = Vx = block_until_keypress()
+    /// Fx0A = Vx = block_until_keypress()
     fn wait_for_keypress(&mut self, x: u8, keys_pressed: &[bool; 16]) {
         match self.is_key_pressed_temp {
             Some(is_key_pressed_og) => {
@@ -323,6 +338,14 @@ impl Cpu {
                 self.program_counter = self.program_counter - 1
             } //Store state before wait
         }
+    }
+    /// Fx15 = dt = Vx
+    fn dt_from_reg(&mut self, x: u8) {
+        self.dt = self.v[x as usize]
+    }
+    /// Fx18 = st = Vx
+    fn st_from_reg(&mut self, x: u8) {
+        self.st = self.v[x as usize]
     }
 }
 
